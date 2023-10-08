b01b8e37435ff0c82816721efd8b88f8446c7b73
Marcelo Amaral Alves <marceloaa@live.com>
2023-10-08 18:55:27 -0400

    Initial Commit

---
+++ iCalories/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iCalories
+//
+//  Created by Marcelo Amaral Alves on 2023-10-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iCalories/iCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Marcelo Amaral Alves on 2023-10-08.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
